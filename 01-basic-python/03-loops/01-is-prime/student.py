9363eefb2804c5127fd05694675831e6108c82a6
NinaKumps <nina.kumps@student.ucll.be>
2021-02-11 23:45:31 +0100

    isPrime

--- 01-basic-python/03-loops/01-is-prime/student.py
+++ 01-basic-python/03-loops/01-is-prime/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def is_prime(n):
+    if n <= 1:
+        return False
+    else:
+        for i in range(2,n):
+            #checking for factor
+            if n % i == 0:
+                return False
+    return True
