b958a9b724c813a8c46bd6b47e0398151863ee15
NinaKumps <nina.kumps@student.ucll.be>
2021-02-13 20:57:46 +0100

    coins

--- 01-basic-python/03-loops/02-coins/student.py
+++ 01-basic-python/03-loops/02-coins/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def coins(one, two, five, goal):
+    #for (i = 0; i < 10; i++)
+    for x in range(0, one + 1):
+        for y in range(0, two + 1):
+            for z in range(0, five + 1):
+                if x + y * 2 + z * 5 == goal:
+                    return True
+    return False
