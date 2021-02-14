29f1fb47e974c58457c6d03ec6f89ce86183dcf1
NinaKumps <nina.kumps@student.ucll.be>
2021-02-14 23:28:33 +0100

    includes

--- 01-basic-python/04-lists/06-includes/student.py
+++ 01-basic-python/04-lists/06-includes/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+def includes(xs, ys):
+    result = all(elem in xs for elem in ys)
+    if result:
+        return True
+    else:
+        return False
+
+#    for y in ys:
+#        if y not in xs:
+#            return False
+#    return True
