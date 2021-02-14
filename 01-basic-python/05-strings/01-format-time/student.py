82e3e17a5eaf9a8671ec06a62b89921ed863b1d3
NinaKumps <nina.kumps@student.ucll.be>
2021-02-14 22:24:44 +0100

    format-time

--- 01-basic-python/05-strings/01-format-time/student.py
+++ 01-basic-python/05-strings/01-format-time/student.py
@@ -1,1 +1,6 @@
 # Write your code here
+def format_time(h, m, s):
+    #result = str(h) + ":" + str(m) + ":" + str(s)
+    result = str(h).rjust(2,"0") + ":" + str(m).rjust(2,"0") + ":" + str(s).rjust(2,"0")
+    #result = f'{h}:{m}:{s}'
+    return result
