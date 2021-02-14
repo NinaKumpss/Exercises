0ea249b3afc08007fbe19be74cbf8e3721d385df
NinaKumps <nina.kumps@student.ucll.be>
2021-02-14 23:07:39 +0100

    word-count

--- 01-basic-python/05-strings/04-word-count/student.py
+++ 01-basic-python/05-strings/04-word-count/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def word_count(string):
+    #removing spaces from start to end
+    zin = string.strip()
+    count = 1
+    for i in zin:
+        #als er een spatie is, count +1 doen
+        if i == " ":
+            count += 1
+    return count
