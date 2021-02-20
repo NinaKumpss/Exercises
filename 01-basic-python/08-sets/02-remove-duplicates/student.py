4a26e54e26b27ee566547f1cb4eaaad6d52d7607
NinaKumps <nina.kumps@student.ucll.be>
2021-02-20 23:10:14 +0100

    remove-duplicates

--- 01-basic-python/08-sets/02-remove-duplicates/student.py
+++ 01-basic-python/08-sets/02-remove-duplicates/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def remove_duplicates(xs):
+    dubbel = set()
+    #bij een list ==> dubbel = []
+    result = []
+    for x in xs:
+        if x not in dubbel:
+            result.append(x)
+            dubbel.add(x)
+    return result
