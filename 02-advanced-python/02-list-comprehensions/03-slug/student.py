3cb0c0adcdc901869b257d5b5fde2d97776e75c4
NinaKumps <nina.kumps@student.ucll.be>
2021-02-22 01:13:44 +0100

    slug

--- 02-advanced-python/02-list-comprehensions/03-slug/student.py
+++ 02-advanced-python/02-list-comprehensions/03-slug/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def slug(name):
+    splitsing = name.split(" ") 
+    voornaam = splitsing[0]
+    plakken = "-".join(splitsing[1:])
+    if len(splitsing) == 1:
+        return voornaam.lower()
+    else:
+        return plakken.lower() + "-" + voornaam.lower()
