77c37d020ae021d9d0536687c0e770bbd9db2c37
NinaKumps <nina.kumps@student.ucll.be>
2021-02-28 15:58:17 +0100

    morse

--- 02-advanced-python/02-list-comprehensions/10-morse/student.py
+++ 02-advanced-python/02-list-comprehensions/10-morse/student.py
@@ -1,1 +1,25 @@
 # Write your code here
+CODE = { 'A':'.-', 'B':'-...',
+   'C':'-.-.', 'D':'-..', 'E':'.',
+   'F':'..-.', 'G':'--.', 'H':'....',
+   'I':'..', 'J':'.---', 'K':'-.-',
+   'L':'.-..', 'M':'--', 'N':'-.',
+   'O':'---', 'P':'.--.', 'Q':'--.-',
+   'R':'.-.', 'S':'...', 'T':'-',
+   'U':'..-', 'V':'...-', 'W':'.--',
+   'X':'-..-', 'Y':'-.--', 'Z':'--..',
+   '1':'.----', '2':'..---', '3':'...--',
+   '4':'....-', '5':'.....', '6':'-....',
+   '7':'--...', '8':'---..', '9':'----.',
+   '0':'-----', ', ':'--..--', '.':'.-.-.-',
+   '?':'..--..', '/':'-..-.', '-':'-....-',
+   '(':'-.--.', ')':'-.--.-'
+}
+
+CODE_REVERSED = {value:key for key,value in CODE.items()}
+
+def to_morse(string):
+    return '   '.join( [ ' '.join( [ CODE[char] for char in word ] ) for word in string.upper().split(' ') ] )
+
+def from_morse(string):
+    return ' '.join( [ ''.join( [ CODE_REVERSED[char] for char in word.split(' ') ] ) for word in string.split('   ') ] )
