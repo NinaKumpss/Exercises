38041db1ab46b78ea7dcdc937e43f50cf06ca38a
NinaKumps <nina.kumps@student.ucll.be>
2021-02-28 15:13:00 +0100

    is-prime

--- 02-advanced-python/02-list-comprehensions/08-is-prime/student.py
+++ 02-advanced-python/02-list-comprehensions/08-is-prime/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+def is_prime(n):
+    #return n > 1 and all( [ n % k != 0 for k in range(2, n) ] )
+
+    if n < 2:
+        return False
+    elif n == 2:
+        return True  
+    for x in range(2, n):
+        if n % x ==0:
+            return False
+    return True
