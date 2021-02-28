dc277d397165fdbce0a1216530373b6a89dc8bea
NinaKumps <nina.kumps@student.ucll.be>
2021-02-28 14:47:35 +0100

    all-greater

--- 02-advanced-python/02-list-comprehensions/07-all-greater/student.py
+++ 02-advanced-python/02-list-comprehensions/07-all-greater/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def all_greater(ns, ms):
+    return all(n >= m for n in ns for m in ms)
+
+
+
+    #if (n for n in ns if n > m for m in ms):
+     #<   return True
+    #return False
