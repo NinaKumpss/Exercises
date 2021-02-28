bbe515fa8f51b219bbb3a523981622f2cd54910a
NinaKumps <nina.kumps@student.ucll.be>
2021-02-28 15:26:14 +0100

    divisors

--- 02-advanced-python/02-list-comprehensions/09-divisors/student.py
+++ 02-advanced-python/02-list-comprehensions/09-divisors/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+def divisors(n):
+   # return [ k for k in range(1, n + 1) if n % k == 0 ]
+    divisors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
+
+    #for x in range(1, n+1):
+    #    if(n % x ==0):
+    #      return x  
