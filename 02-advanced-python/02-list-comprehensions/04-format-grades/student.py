c674abd4fb5864730f2769b9de337f8c2ec48351
NinaKumps <nina.kumps@student.ucll.be>
2021-03-03 01:44:58 +0100

    format-grades

--- 02-advanced-python/02-list-comprehensions/04-format-grades/student.py
+++ 02-advanced-python/02-list-comprehensions/04-format-grades/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def format_grades(grades):
+    avgDict = {}
+    for k,v in grades.items():
+    # v is the list of grades for student k
+        avgDict[k] = round(sum(v)/ float(len(v)))
+
+    res = '\n'.join(key + ": " + str(val) for key, val in avgDict.items()) 
+    return res
