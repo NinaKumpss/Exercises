8a8ae3052a2314f879e27e66937dadae46f2092f
NinaKumps <nina.kumps@student.ucll.be>
2021-04-10 22:48:11 +0200

    lyrics

---
+++ 04-scripting/02-io/05-lyrics/student.py
@@ -0,0 +1,18 @@
+import sys
+import urllib.request
+import json
+import re
+
+def to_url(string):
+    #URLs cannot contain spaces. Replaces each space by %20.
+    return re.sub(' ', '%20', string)
+
+artist, title = sys.argv[1:]
+title = to_url(title)
+artist = to_url(artist)
+
+url = f"https://api.lyrics.ovh/v1/{artist}/{title}"
+
+with urllib.request.urlopen(url) as input:
+    contents = json.loads(input.read())
+    print(data['lyrics'])
