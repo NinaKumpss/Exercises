fa576cbf39e0256e4ff5c7dee7b8f8dcf975f060
NinaKumps <nina.kumps@student.ucll.be>
2021-04-24 15:14:12 +0200

    alles gecommit

--- 03-regex/02-capturing/01-parse-time/student.py
+++ 03-regex/02-capturing/01-parse-time/student.py
@@ -1,1 +1,6 @@
 # Write your code here
+import re
+def parse_time(string):
+    match = re.fullmatch(r'(\d{2}):(\d{2}):(\d{2})(\.\d{3})?', string)
+    if match:
+        h, m, s, ms = match.groups('000')
