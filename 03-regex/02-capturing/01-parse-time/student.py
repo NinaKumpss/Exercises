20fc702b8a47bacc99b261a328fd7f2364e02a2a
NinaKumps <nina.kumps@student.ucll.be>
2021-04-25 13:10:29 +0200

    parse-time

--- 03-regex/02-capturing/01-parse-time/student.py
+++ 03-regex/02-capturing/01-parse-time/student.py
@@ -1,6 +1,8 @@
 # Write your code here
 import re
 def parse_time(string):
-    match = re.fullmatch(r'(\d{2}):(\d{2}):(\d{2})(\.\d{3})?', string)
+    match = re.fullmatch(r'(\d{2}):(\d{2}):(\d{2})(?:\.(\d{3}))?', string)
     if match:
-        h, m, s, ms = match.groups('000')
+        return tuple( [ int(x) for x in match.groups('000') ] )
+    else:
+        return None
