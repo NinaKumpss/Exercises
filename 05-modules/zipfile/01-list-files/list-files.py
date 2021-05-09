630b693566f573044bfa01c6648b5db440a9d153
NinaKumps <nina.kumps@student.ucll.be>
2021-05-09 22:32:45 +0200

    list-files

--- 05-modules/zipfile/01-list-files/list-files.py
+++ 05-modules/zipfile/01-list-files/list-files.py
@@ -8,4 +8,5 @@ with zipfile.ZipFile(filename, 'r') as myzip:
     for file in myzip.namelist():
         print(file)
     #of
+    #print(dir(file)) ==> met dir() kan je de mogelijkheden zien
     #print("\n".join(myzip.namelist())) alles op nieuwe lijn
